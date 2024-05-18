91fae5bc4dcd8494d8eedb7025d81e34cb1ab335
Erick <19063989@student.hhs.nl>
2024-05-18 09:49:42 +0200

    Added day and night rotate

---
+++ Assets/DayNightTime.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class DayNightTime : MonoBehaviour
+{
+    public float rotationSpeed = 10f; // Speed of rotation
+    private bool rotatingForward = true; // Direction of rotation
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+        float currentXRotation = transform.localEulerAngles.x;
+
+        // Adjust the current rotation angle to be between -180 and 180 degrees
+        if (currentXRotation > 180f)
+        {
+            currentXRotation -= 360f;
+        }
+
+        // Check if the rotation has reached the limits and reverse direction
+        if (currentXRotation >= 50f)
+        {
+            rotatingForward = false; // Reverse direction to backward
+        }
+        else if (currentXRotation <= -10f)
+        {
+            rotatingForward = true; // Reverse direction to forward
+        }
+
+        // Apply the rotation based on the current direction
+        if (rotatingForward)
+        {
+            transform.Rotate(Vector3.right * rotationAmount);
+        }
+        else
+        {
+            transform.Rotate(Vector3.left * rotationAmount);
+        }
+    }
+}
