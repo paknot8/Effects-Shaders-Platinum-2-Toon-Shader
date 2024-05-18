5c3c65346b7ab33ee96c2e82665c063598e2de9b
Erick <19063989@student.hhs.nl>
2024-05-18 16:55:22 +0200

    Added different Toon Shaders

--- Assets/DayNightTime.cs
+++ Assets/DayNightTime.cs
@@ -1,40 +1,51 @@
 using UnityEngine;
+using TMPro;
 
 public class DayNightTime : MonoBehaviour
 {
-    private readonly float rotationSpeed = 30f; // Speed of rotation
+    public float rotationSpeed = 30f; // Speed of rotation
     private bool rotatingForward = true; // Direction of rotation
+    private bool isRotating = true; // Toggle rotation on and off
 
-    // Update is called once per frame
     void Update()
     {
-        float rotationAmount = rotationSpeed * Time.deltaTime;
-        float currentXRotation = transform.localEulerAngles.x;
+        // Only apply rotation if isRotating is true
+        if (isRotating)
+        {
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            float currentXRotation = transform.localEulerAngles.x;
 
-        // Adjust the current rotation angle to be between -180 and 180 degrees
-        if (currentXRotation > 180f)
-        {
-            currentXRotation -= 360f;
-        }
+            // Adjust the current rotation angle to be between -180 and 180 degrees
+            if (currentXRotation > 180f)
+            {
+                currentXRotation -= 360f;
+            }
 
-        // Check if the rotation has reached the limits and reverse direction
-        if (currentXRotation >= 100f)
-        {
-            rotatingForward = false; // Reverse direction to backward
-        }
-        else if (currentXRotation <= -100f)
-        {
-            rotatingForward = true; // Reverse direction to forward
-        }
+            // Check if the rotation has reached the limits and reverse direction
+            if (currentXRotation >= 100f)
+            {
+                rotatingForward = false; // Reverse direction to backward
+            }
+            else if (currentXRotation <= -100f)
+            {
+                rotatingForward = true; // Reverse direction to forward
+            }
 
-        // Apply the rotation based on the current direction
-        if (rotatingForward)
-        {
-            transform.Rotate(Vector3.right * rotationAmount);
+            // Apply the rotation based on the current direction
+            if (rotatingForward)
+            {
+                transform.Rotate(Vector3.right * rotationAmount);
+            }
+            else
+            {
+                transform.Rotate(Vector3.left * rotationAmount);
+            }
         }
-        else
-        {
-            transform.Rotate(Vector3.left * rotationAmount);
-        }
+    }
+
+    // Public method to toggle rotation
+    public void ToggleRotation()
+    {
+        isRotating = !isRotating;
     }
 }
