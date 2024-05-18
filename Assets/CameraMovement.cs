c42a5268db33609fb2167852bbaa3f64cdcffe68
Erick <19063989@student.hhs.nl>
2024-05-18 09:26:23 +0200

    Added Camera Movement

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private readonly float speed = 7f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the horizontal and vertical input (WASD keys)
+        float horizontal = Input.GetAxis("Horizontal"); // A and D keys
+        float vertical = Input.GetAxis("Vertical"); // W and S keys
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        movement = speed * Time.deltaTime * movement.normalized;
+
+        // Check for vertical movement (Space and Left Ctrl keys)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            movement.y += speed * Time.deltaTime; // Move up
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            movement.y -= speed * Time.deltaTime; // Move down
+        }
+
+        // Apply the movement to the camera's position
+        transform.Translate(movement, Space.World);
     }
 }
